75a1af15507a9c205a9f0829c799b5191a09a73c
cFadness <codie1989@gmail.com>
2021-11-21 19:21:38 -0800

    Make Get method for all songs.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const repoContext = require('./repository/repository-wrapper');
 
 const app = express();
 
 app.listen(3000, function () {
     console.log("Server started. Listening on port 3000.");
+});
+
+app.get('/api/songs', (req, res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
 });
 
